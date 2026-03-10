0c5d9c3766fb43631bdd92ce3acc029cbbebd45f
Porco Rosso <porcorossobaojie@gmail.com>
2026-03-10 23:38:11 +0800

    基本完成backtest

--- src/quanta/libs/_flow/_cap/core.py
+++ src/quanta/libs/_flow/_cap/core.py
@@ -10,7 +10,7 @@ from functools import lru_cache
 from quanta.libs._flow._cap.base import Series, DataFrame
 
 class Unit():
-    def __init__(self, signal=None, settle=None, target=None, cash=None, trade_cost=None):
+    def __init__(self, signal=None, settle=None, target=None, cash=None, trade_cost=True):
         series_instance = Series._config.recommand_settings.to_dict()
         series_instance.update(
             {k: v for k, v in [('cash', cash), ('trade_cost', trade_cost)] if v is not None}
@@ -19,24 +19,23 @@ class Unit():
         params = {i:j for i, j in params.items() if j is not None}
         if not len(params):
             raise ValueError(f"parameters:<{params}> can not be all None...")
-        
-        if signal is not None:
-            self.signal = signal
+
+        if settle is not None and signal is None:
+            self.signal = Series(
+                [],
+                index = pd.CategoricalIndex([], name=settle.index.name),
+                name = settle.name,
+                state = 'signal',
+                unit = 'share',
+                cash = 0
+            )
         else:
-            if settle is not None:
-                self.signal = Series(
-                    [], 
-                    index = pd.CategoricalIndex([], name=settle.index.name),
-                    name = settle.name,
-                    state = 'signal',
-                    unit = 'share',
-                    cash = 0
-                )
-                
+            self.signal = signal
+
         if settle is None:
             obj = signal if signal is not None else target
             settle = Series(
-                [], 
+                [],
                 index = pd.CategoricalIndex([], name=obj.index.name),
                 name = obj.name,
                 state = 'settle',
@@ -51,9 +50,10 @@ class Unit():
             if target.name is None:
                 target.name = self.settle.name
         self._target = target
-        
+        self._trade_cost = trade_cost
+
         self._meta_attrs = series_instance | {'signal':signal, 'settle':settle, 'target':target}
-        
+
     @property
     def signal(self):
         return self._signal
@@ -66,21 +66,21 @@ class Unit():
         else:
             v = Series(v, state='signal', unit='share')
         setattr(self, '_signal', v)
-    
+
     @property
     def order(self):
         return self.signal.order() if self.signal is not None else None
-        
+
     @property
     def trade(self):
         return self.order.trade() if self.signal is not None else None
-    
+
     @property
     def entrade(self):
         x = self.trade
         x = (x.enbuy() + x.ensell()) if self.signal is not None else None
         return x
-    
+
     @property
     def settle(self):
         return self._settle
@@ -89,24 +89,24 @@ class Unit():
         if not isinstance(v, Series):
             v = Series(v, state='settle', unit='share')
         self._settle = v
-        
+
     @property
     def trade_cost(self):
         return self.entrade.trade_cost(trade_check=False) if self.signal is not None else 0
-    
+
     @lru_cache(maxsize=16)
-    def roll(self, trade_cost=True):
+    def roll(self):
         if self.signal is not None:
             settle = self.settle.share()
             entrade = self.entrade.share()
             settle = settle + entrade
             settle = settle.settle()
-            if trade_cost:
+            if self._trade_cost:
                 settle.cash = settle.cash + self.trade_cost
         else:
             settle = self.settle.f.day_shift(1)
         return settle
-    
+
     @property
     def target(self):
         if self._target is not None:
@@ -116,25 +116,41 @@ class Unit():
             target = roll.share(roll.total_assets() - self.trade_cost)
             target.cash = 0
             return target
-    
+
     def _target_setter(self, v):
         roll = self.roll()
         x = Series(v, state='settle', unit='weight').weight().share(roll.total_assets())
         self._target = x
-        
+
     def __call__(self, new_target):
         settle = self.roll()
         self._target_setter(new_target)
         signal = self.target - settle
         x = Unit(signal=signal, settle=settle, target=None)
         return x
-    
 
-''' 
+class Chain:
+    def __init__(self, dataframe, cash=10000, trade_cost=True):
+        self.cash = cash
+        self.trade_cost = trade_cost
+        self._obj = dataframe
+    def __call__(self):
+        dic = {}
+        unit_obj = Unit(target=self._obj.iloc[0], cash=self.cash, trade_cost=self.trade_cost)
+        dic[unit_obj.settle.name] = unit_obj
+        for i,j in self._obj.iloc[1:].iterrows():
+            if i.month != unit_obj.settle.name.month:
+                print(i)
+            dic[i] = unit_obj(j)
+
+        return dic
+
+
+'''
 from quanta import flow
 ret = flow.astock('ret')
 series = Series(ret.iloc[200, :200]).abs().share(1000)
-self = Unit(signal=series, cash=1000)
+self = Unit(signal=eries(ret.iloc[200, :200]).abs().share(1000), cash=1000)
 new_target = Series(ret.iloc[300, 100:300]).abs()
 g1 = self(new_target)
 print(1)
