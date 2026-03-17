1db5810f609320792f3be0bde2530ca30de25cc4
Porco Rosso <porcorossobaojie@gmail.com>
2026-03-17 20:12:05 +0800

    temp

--- src/quanta/factors/barra/use4.py
+++ src/quanta/factors/barra/use4.py
@@ -12,16 +12,16 @@ import pandas as pd
 from quanta import flow
 
 from quanta.factors.barra._base import main as meta
-#from._base import main as meta
+#from ._base import main as meta
 
 class main(meta):
     _model_name = 'use4'
-    
+
     @classmethod
     @lru_cache(maxsize=4)
     def momentum(
-        cls, 
-        long_periods = 504, 
+        cls,
+        long_periods = 504,
         short_periods = 21,
         halflife = 126,
         bench='full',
@@ -29,11 +29,12 @@ class main(meta):
 
     ):
         bench = cls.bench(bench).tools.log()
-        ret = getattr(flow, portfolio_type)(cls.ret).tools.log()
+        ret = getattr(flow, portfolio_type)(cls.returns).tools.log()
         ret_values = ret.values
         ret_bool = ret.notnull()
         w = 0.5 ** (np.arange(long_periods+short_periods)[::-1] / halflife)
         w = w / w.sum()
         w_matrix = w.repeat(ret.shape[1]).reshape(-1, ret.shape[1])
-        
-                
+
+
+
