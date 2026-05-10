634024295cc5dfbaddd364c3a457cb46c5b935cb
Porco Rosso <porcorossobaojie@gmail.com>
2026-05-10 23:26:56 +0800

    update cn6 factors

--- src/quanta/faclib/barra/_base.py
+++ src/quanta/faclib/barra/_base.py
@@ -130,8 +130,8 @@ class main(meta):
         w = pd.tools.halflife(periods, halflife).astype('float32')
         beta = ret.stats.neutral(beta=bench, neu_axis=0, w=w, periods=periods)
         alpha, beta, resid = (
-            beta.params.const, 
-            beta.params.beta, 
+            beta.params.const,
+            beta.params.beta,
             pd.DataFrame(
                 beta.resid.std(beta.resid.values),
                 index = beta.resid.index,
@@ -265,7 +265,7 @@ class main(meta):
         df = x.rolling(periods).apply(lambda x: np.max(x[location], axis=0) - np.min(x[location], axis=0), raw=True)
         df = df.tools.log().f.tradestatus(periods=periods)
         return df
-    
+
     @classmethod
     @lru_cache(maxsize=4)
     def _turnover(cls, periods, portfolio_type: str = 'astock') -> pd.DataFrame:
@@ -273,24 +273,24 @@ class main(meta):
         x = x.rolling(periods, min_periods=periods // 4).sum()
         x = x.tools.log()
         return x
-    
+
     @classmethod
     def month_turnover(cls, periods=21, portfolio_type: str = 'astock') -> pd.DataFrame:
         x = cls._turnover(periods=periods, portfolio_type = portfolio_type)
         return x
-    
+
     @classmethod
     def quarter_turnover(cls, periods=63, portfolio_type: str = 'astock') -> pd.DataFrame:
         x = cls._turnover(periods=periods, portfolio_type = portfolio_type)
         x = x - np.log(4)
         return x
-    
+
     @classmethod
     def annual_turnover(cls, periods=252, portfolio_type: str = 'astock') -> pd.DataFrame:
         x = cls._turnover(periods=periods, portfolio_type = portfolio_type)
         x = x - np.log(12)
-        return x    
-    
+        return x
+
     @classmethod
     @lru_cache(maxsize=4)
     def annul_weight_turnover(cls, periods=252, portfolio_type: str = 'astock') -> pd.DataFrame:
@@ -299,7 +299,7 @@ class main(meta):
         x = df.gen.roll_weight(w)
         x = x[df.notnull()]
         return x
-    
+
     @classmethod
     @lru_cache(maxsize=4)
     def short_term_reversal(cls, periods=21, portfolio_type: str = 'astock') -> pd.DataFrame:
@@ -308,30 +308,143 @@ class main(meta):
         x = ret.gen.roll_weight(w)
         return x
 
-    @classmethod    
+    @classmethod
+    @lru_cache(maxsize=4)
     def seasonal(cls, periods=5, window=252, future=21, portfolio_type: str = 'astock') -> pd.DataFrame:
         ret = getattr(flow, portfolio_type)(cls.trade.returns).f.tradestatus()
         ret = ret.rolling(future).mean().shift(window - future + 1)
         df = {i:ret.shift(i * window) for i in range(periods)}
         df = pd.concat(df, axis=1).stack().mean(axis=1).unstack().reindex_like(ret).f.tradestatus()
         return df
-    
 
-    
-    
-        
+    @classmethod
+    @lru_cache(maxsize=4)
+    def industry_momentum(cls, periods=126, industry_code = 'swl1_name', mv_rebalance=True):
+        w = pd.tools.halflife(periods, periods // 6)
+        mv = flow.astock(cls.finance.cur_mv) ** 0.5 *1e4
+        ret = flow.astock(cls.trade.returns).f.tradestatus()
+        x = ret.gen.roll_weight(w)
+        x = x * mv
+        mv = mv.f.label(industry_code).groupby(industry_code, axis=1).sum(min_count=1).f.expand()
+        mv = mv.groupby(cls.trade.astock_code, axis=1).sum(min_count=1)
+        i_mom = x.f.label(industry_code)
+        i_mom = i_mom.groupby(industry_code, axis=1).sum(min_count=1).f.expand()
+        i_mom = i_mom.groupby(cls.trade.astock_code, axis=1).sum(min_count=1)
+        mom = (i_mom - x)
+        if mv_rebalance:
+            mom = mom / mv
+        return mom
 
-        
-        
-        
-        
-        
-        
-    
-    
-        
-        
-        
-        
-    
-    
+    @classmethod
+    @lru_cache(maxsize=4)
+    def relative_strengh(cls, periods = 252, shift=11):
+        w = pd.tools.halflife(periods, periods // 2)
+        ret = flow.astock(cls.trade.returns).f.tradestatus()
+        x = ret.gen.roll_weight(w)
+        x = x.rolling(shift).mean().shift(shift)
+        return x
+
+    @classmethod
+    def historical_alpha(
+        cls,
+        periods: int = 252,
+        halflife: int = None,
+        bench: str = 'full',
+        portfolio_type: str = 'astock'
+    ) -> pd.DataFrame:
+        return cls._beta(periods=periods, halflife=halflife, bench=bench, portfolio_type=portfolio_type).alpha
+
+    @classmethod
+    @lru_cache(maxsize=1)
+    def market_leverage(cls):
+        mv = flow.astock(cls.finance.val_mv) * 1e8
+        long_liab = flow.astock.finance(cls.finance.long_liab, shift=4, periods=1)
+        x = (mv + long_liab) / long_liab
+        return x
+
+    @classmethod
+    @lru_cache(maxsize=1)
+    def book_leverage(cls):
+        mv = flow.astock(cls.finance.val_mv) * 1e8
+        long_liab = flow.astock.finance(cls.finance.long_liab, shift=4, periods=1)
+        pb = flow.astock(cls.finance.val_mv)
+        x = pb ** -1 + (long_liab / mv)
+        return x
+
+    @classmethod
+    @lru_cache(maxsize=1)
+    def debt_to_asset_ratio(cls):
+        liab = flow.astock.finance(cls.finance.total_liab, periods=1, shift=4)
+        asset = flow.astock.finance(cls.finance.total_assets, periods=1, shift=4)
+        x = liab / asset
+        return x
+
+    @classmethod
+    @lru_cache(maxsize=4)
+    def _variation(cls, key, periods, quarter_adj):
+        df = flow.astock.finance(key, shift=4, periods=periods, quarter_adj=quarter_adj)
+        df = df / df.groupby(cls.trade.trade_dt).transform('mean').replace(0, np.nan)
+        df = df.groupby(cls.trade.trade_dt).std()
+        return df
+
+    @classmethod
+    def variation_in_sales(cls, periods=20):
+        df = cls._variation(cls.finance.oper_rev, periods, 3)
+        return df
+
+    @classmethod
+    def variation_to_earnings(cls, periods=20):
+        df = cls._variation(cls.finance.net_profit, periods, 3)
+        return df
+
+    @classmethod
+    def variation_to_cashflow(cls, periods=20):
+        df = cls._variation(cls.finance.net_cashflow, periods, None)
+        return df
+
+    @classmethod
+    @lru_cache(maxsize=1)
+    def accr_balancesheet(cls):
+        da = pd.concat(
+            {i: flow.astock.finance(i, periods=1, shift=8) for i in
+                 [cls.finance.deprec, cls.finance.inv_depre, cls.finance.inta_amort, cls.finance.long_amort]
+            },
+            axis=1
+        )
+        da = da.groupby(cls.trade.astock_code, axis=1).sum(min_count=1).fillna(0).f.tradestatus()
+        ta = flow.astock.finance(cls.finance.total_assets, periods=2, shift=4).swaplevel(-1,0)
+        tc = flow.astock.finance(cls.finance.cash_and_equity, periods=2, shift=4).swaplevel(-1,0)
+        x = (ta.loc[0] - ta.loc[-1]) - (tc.loc[0] - tc.loc[-1])
+        x = (x - da) / ta.loc[0]
+        return x
+
+    @classmethod
+    @lru_cache(maxsize=1)
+    def accr_cashflow(cls):
+        da = pd.concat(
+            {i: flow.astock.finance(i, periods=1, shift=8) for i in
+                 [cls.finance.deprec, cls.finance.inv_depre, cls.finance.inta_amort, cls.finance.long_amort]
+            },
+            axis=1
+        )
+        da = da.groupby(cls.trade.astock_code, axis=1).sum(min_count=1).fillna(0).f.tradestatus()
+        ni = flow.astock.finance(cls.finance.net_profit, shift=4, periods=1, quarter_adj=3)
+        cfo = flow.astock.finance(cls.finance.oper_cash, shift=4, periods=1, quarter_adj=3)
+        cfi = flow.astock.finance(cls.finance.inv_cash, shift=4, periods=1, quarter_adj=3)
+        ta =flow.astock.finance(cls.finance.total_assets, periods=1, shift=4)
+        x = ni - (cfo + cfi) + da
+        x = x / ta
+        return x
+
+
+
+
+
+
+
+
+
+
+
+
+
