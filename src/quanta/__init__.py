99e5a970055931516ccf901727d997bd944a49c6
Porco Rosso <porcorossobaojie@gmail.com>
2026-05-12 17:23:05 +0800

    version update

--- src/quanta/__init__.py
+++ src/quanta/__init__.py
@@ -4,4 +4,4 @@ from .libs import _flow as flow
 from . import faclib 
 from .trade.account.main import main as account
 from .strategys import meta as strategys
-__version__ = "0.8.1"
+__version__ = "0.8.2"
