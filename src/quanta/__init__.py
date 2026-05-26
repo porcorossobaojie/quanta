5f885a031d453385abcc5c4313bdce8177469082
Porco Rosso <porcorossobaojie@gmail.com>
2026-05-26 22:51:34 +0800

    fix bugs

--- src/quanta/__init__.py
+++ src/quanta/__init__.py
@@ -1,12 +1,12 @@
 from .libs import _pandas
 
 try:
+    from .libs import _flow as flow
     from .trade.account.main import main as account
     from .strategys import meta as strategys
-    from .libs import _flow as flow
-    from . import config, libs, data    
+    from . import config, libs, data
     from . import faclib
 except:
     pass
-    
+
 __version__ = "0.8.2"
