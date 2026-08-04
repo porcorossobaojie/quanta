2ff7e79c0fe2b04d137868b63cc4dda528ba44ff
Porco Rosso <porcorossobaojie@gmail.com>
2026-08-04 18:04:20 +0800

    update minfreq

--- src/quanta/data/joinquant/meta/minute_main.py
+++ src/quanta/data/joinquant/meta/minute_main.py
@@ -8,6 +8,7 @@ Created on Sun Aug  2 16:37:57 2026
 from typing import Any, Dict, List, Union, Optional
 import numpy as np
 import pandas as pd
+import duckdb
 import jqdatasdk as jq
 
 from quanta.libs.utils import merge_dicts
@@ -16,9 +17,8 @@ from quanta.libs.db.main import main as db
 
 config = settings('data')
 
-db.__setting__(database=config.public_keys.minfreq_settings.key.database)
 
-class main(db, type('recommand_settings', (), config.public_keys.minfreq_settings.key)):
+class main(type('recommand_settings', (), config.public_keys.minfreq_settings.key), db):
     """
     ===========================================================================
     Base metadata and connection class for JoinQuant data extraction,
